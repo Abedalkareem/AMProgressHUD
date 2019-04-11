672fcdb9ff7de6057f723c308583c3c267589861
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2019-04-11 10:48:11 +0400

    fix the pod example

--- Example/AMProgressHUD/ViewController.swift
+++ Example/AMProgressHUD/ViewController.swift
@@ -7,18 +7,21 @@
 //
 
 import UIKit
+import AMProgressHUD
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+  }
 
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    AMProgressHUD.show()
+  }
+  
 }
 
